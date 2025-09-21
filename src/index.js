ed7d2fbd3a41766d4dbd4c209df46ca7b7990372
Junior <infojr.83@gmail.com>
2025-09-20 20:05:53 -0700

    sign up update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// src/index.js
-import "./styles/global.css";
-
 
 import App from "./App";
-
 import { AuthProvider } from "./context/AuthContext";
 import { initBootstrapUserDoc } from "./services/bootstrapUserDoc";
 
@@ -15,9 +11,41 @@ import { initBootstrapUserDoc } from "./services/bootstrapUserDoc";
 import "./styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// src/index.js (or src/App.js – either is fine)
 
+/**
+ * Hard-unregister any service workers so users don't get a stale cached bundle
+ * (which is what causes the `key=YOUR_KEY` issue to persist on some devices).
+ */
+(function killServiceWorkersAndOldTokens() {
+  if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .getRegistrations()
+      .then((regs) => regs.forEach((r) => r.unregister()))
+      .catch(() => {});
 
+    // Ask any active SW to stop controlling the page ASAP
+    navigator.serviceWorker
+      .getRegistrations()
+      .then((regs) => regs.forEach((r) => r.active && r.active.postMessage({ type: "SKIP_WAITING" })))
+      .catch(() => {});
+
+    // Optional: clear old Workbox caches (best-effort)
+    if (window.caches && caches.keys) {
+      caches.keys().then((keys) => {
+        keys.forEach((k) => {
+          if (/workbox|webpack|runtime|precache/i.test(k)) {
+            caches.delete(k);
+          }
+        });
+      });
+    }
+  }
+
+  // Also clear any leftover App Check debug token in prod
+  try {
+    localStorage.removeItem("firebase:appCheckDebugToken");
+  } catch (_) {}
+})();
 
 // Ensure user doc + default emailPrefs exist on first sign-in
 initBootstrapUserDoc();
